5ce4f1fa88b37aaaf6a6a6b20f6fc98f44bb773f
fearMOV <fear.mov@gmail.com>
2019-04-02 11:28:32 +0500

    Убрана передача информации о бд из аргументов в конфигурационынй файл

--- MyDemon.py
+++ MyDemon.py
@@ -10,63 +10,50 @@ import redis
 import sys
 import json
 import consul
-import daemon
-import time
+# import daemon
+# import time
 
 
 # Функции---------------------------------------------------------------------------------------------------------------
-'''def data_conn(type_db):
+def parser_config(*argvs):
     """
-    Преобразуем аргументы консоли в словарь.
+    Парсим конфигурационный файл.
 
-    :param type_db: строковая переменная, обозначающая тип БД (redis/psql), для которой составляется словарь.
-    :return: возвращает словарь.
+    :param argvs: принимает аргументы из командной строки
+    нужно для поиска -c, после которой указвается путь к конфигурационному файлу
+
+    :return: возвращает словарь с конфигом
     """
-    data = {}
-    for argv in sys.argv:
-        argv = argv.split("=")
-        if type_db == "psql" and argv[0] == type_db:
-            params = argv[1].split(":")
-            data["dbname"] = params[0]
-            data["host"] = params[1]
-            data["user"] = params[2]
-            data["password"] = params[3]
-        elif type_db == "redis" and argv[0] == type_db:
-            params = argv[1].split(":")
-            data["host"] = params[0]
-            data["port"] = params[1]
-            data["db"] = params[2]
-        elif type_db == "consul" and argv[0] == type_db:
-            params = argv[1].split(":")
-            data["host"] = params[0]
-            data["port"] = params[1]
-            data["dc"] = params[2]
-    return data'''
-
-
-def parser_config():
-    pass
+    index = 0
+    if "-c" in argvs:
+        index = argvs.index("-c") + 1
+    with open("config.json" if index == 0 else argvs[index]) as inf:
+        config_json = json.load(inf)
+    return config_json
 
 
 def main():
+    """
+    Собираем json файл, беря данные из Redis и PostgreSQL, и кладём в Consul
+
+    :return: Ничего не возвращает
+    """
     global k
 
     # Переменная для формирования JSON файла
-    dataJson = {}
+    data_json = {}
 
     # Создаём словари для подключенний к PostgreSQL, Redis, Consul
-    dataConnPostgreSQL = data_conn("psql")
-    dataConnRedis = data_conn("redis")
-    dataConnConsul = data_conn("consul")
+    data_conn_db = parser_config(sys.argv)
 
     # Подключаемся к PostgreSQL и делаем запросы
     with psycopg2.connect(
-            dbname=dataConnPostgreSQL["dbname"], host=dataConnPostgreSQL["host"],
-            user=dataConnPostgreSQL["user"], password=dataConnPostgreSQL["password"]
+            dbname=data_conn_db["postgresql"]["dbname"], host=data_conn_db["postgresql"]["host"],
+            user=data_conn_db["postgresql"]["user"], password=data_conn_db["postgresql"]["password"]
     ) as conn:
         with conn.cursor(cursor_factory=DictCursor) as cursor:
             cursor.execute('SELECT * FROM naumb_version')
-            dataJson["naumb_version"] = []
+            data_json["naumb_version"] = []
             naumb_version = {}
             i = 0
             for row in cursor:
@@ -78,65 +65,74 @@ def main():
                 key = "naumb_version" + str(i)
                 naumb_version[key] = data
                 print(row)
-            dataJson["naumb_version"].append(naumb_version)
+            data_json["naumb_version"].append(naumb_version)
 
             cursor.execute('SELECT * FROM naumb_file_type')
             naumb_file_type = {}
             for row in cursor:
                 print(row)
                 naumb_file_type[row[0]] = row[1]
-            dataJson["naumb_file_type"] = naumb_file_type
+            data_json["naumb_file_type"] = naumb_file_type
 
             cursor.execute("SELECT count(*) FROM call_legs")
             kolvo_call_legs = (cursor.fetchone())
-            dataJson["kolvo_call_legs"] = kolvo_call_legs[0]
+            data_json["kolvo_call_legs"] = kolvo_call_legs[0]
 
     # Подключаемся к Redis и делаем запросы
-    connRedis = redis.StrictRedis(host=dataConnRedis["host"], port=dataConnRedis["port"], db=dataConnRedis["db"])
+    conn_redis = redis.StrictRedis(
+        host=data_conn_db["redis"]["host"],
+        port=data_conn_db["redis"]["port"],
+        db=data_conn_db["redis"]["db"]
+    )
 
-    g = connRedis.smembers("online_agents")
+    g = conn_redis.smembers("online_agents")
     print(g)
 
-    online_agents = connRedis.hgetall("online_agents:substate:normal")
+    online_agents = conn_redis.hgetall("online_agents:substate:normal")
     new_online_agents = {}
     for item in online_agents.items():
         key, value = map(str, item)
         new_online_agents[key] = value
-    dataJson["online_agents:substate:normal"] = new_online_agents
+    data_json["online_agents:substate:normal"] = new_online_agents
 
     # Создаём json
-    dataJson["param"] = k
-    my_json = json.dumps(dataJson, indent=4)
+    data_json["param"] = k
+    my_json = json.dumps(data_json, indent=4)
 
     # Закидывем json в consul
-    c = consul.Consul(host=dataConnConsul["host"], port=dataConnConsul["port"], dc=dataConnConsul["dc"])
+    c = consul.Consul(
+        host=data_conn_db["consul"]["host"],
+        port=data_conn_db["consul"]["port"],
+        dc=data_conn_db["consul"]["dc"]
+    )
     print(c.kv.put("log", my_json))
 
 
 # Тело программы--------------------------------------------------------------------------------------------------------
 # Техническая переменная для отслеживания количества перезаписей
 k = 0
-
+"""
 if "-d" in sys.argv:
     with daemon.DaemonContext():
         while True:
             k += 1
             main()
             time.sleep(30)
-else:
-    main()
+else:"""
+main()
 
 # Альфа версия (Не смотреть)--------------------------------------------------------------------------------------------
+"""
 # Расчетное время ожидания (EWT) по каждой очереди
-# project_ids = connRedis.get("project_config:projects_set")
-# for project_id in project_ids:
-    # s = connRedis.get("project_config:%s:mean_wait" % project_id)
-    # print(s)
+project_ids = connRedis.get("project_config:projects_set")
+for project_id in project_ids:
+    s = connRedis.get("project_config:%s:mean_wait" % project_id)
+    print(s)
 
 
 
 # Расчёт уровня сервиса SL
-"""with psycopg2.connect(
+with psycopg2.connect(
     dbname=dataConnPostrgeSQL["dbname"], host=dataConnPostrgeSQL["host"],
     user=dataConnPostrgeSQL["user"], password=dataConnPostrgeSQL["password"]
 ) as conn:
@@ -152,6 +148,6 @@ else:
                             FROM ns_inbound_call_data \
                             WHERE is_unblocked=True and is_shortly_abandoned=False \
                             GROUP BY project_id"
-        )"""
-        # print(str(int(x)/int(y)))
-
+        )
+        print(str(int(x)/int(y)))
+"""
