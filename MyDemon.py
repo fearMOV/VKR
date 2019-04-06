c57edd1ff3b39b8ab212ecd4ff490516e5247cd3
fearMOV <fear.mov@gmail.com>
2019-04-07 02:27:06 +0500

    Настроены первые 3 запроса и сделан json файл в нужном формате.

--- MyDemon.py
+++ MyDemon.py
@@ -37,7 +37,7 @@ def parser_config(*argvs):
 
 def decode_bytes_in_str(x):
     x = str(x)
-    return x[2:len(x)-1]
+    return x[2:len(x) - 1]
 
 
 def main():
@@ -60,50 +60,35 @@ def main():
         raise RuntimeError
     except RuntimeError:
         log.error("Error!")
-
-    # Переменная для формирования JSON файла
-    data_json = {}
+    # cursor_factory=DictCursor
     # Подключаемся к PostgreSQL и делаем запросы
     with psycopg2.connect(**config["postgresql"]) as conn:
-        with conn.cursor(cursor_factory=DictCursor) as cursor:
-            cursor.execute('SELECT * FROM naumb_version')
-            data_json["naumb_version"] = []
-            naumb_version = {}
-            i = 0
-            for row in cursor:
-                data = {}
-                data["repository_id"] = row[0]
-                data["repository_path"] = row[1]
-                data["version"] = row[2]
-                i += 1
-                key = "naumb_version" + str(i)
-                naumb_version[key] = data
-                print(row)
-            data_json["naumb_version"].append(naumb_version)
-
-            cursor.execute('SELECT * FROM naumb_file_type')
-            naumb_file_type = {}
+        with conn.cursor() as cursor:
+            # Расчёт уровня сервиса SL
+            cursor.execute(
+                "SELECT project_id, count(session_id) \
+                FROM ns_inbound_call_data \
+                WHERE is_processed=True and is_processed_after_threshold=False \
+                GROUP BY project_id"
+            )
+            timed_answering_calls = {key: value for key, value in cursor}
             for row in cursor:
                 print(row)
-                naumb_file_type[row[0]] = row[1]
-            data_json["naumb_file_type"] = naumb_file_type
 
-            cursor.execute("SELECT count(*) FROM call_legs")
-            kolvo_call_legs = (cursor.fetchone())
-            data_json["kolvo_call_legs"] = kolvo_call_legs[0]
+            cursor.execute(
+                "SELECT project_id, count(session_id) \
+                FROM ns_inbound_call_data \
+                WHERE is_unblocked=True and is_shortly_abandoned=False \
+                GROUP BY project_id"
+            )
+            queued_calls_without_irrelevant_missed_calls = {key: value for key, value in cursor}
+            sl_projects = {key: value / queued_calls_without_irrelevant_missed_calls[key] for key, value in
+                           timed_answering_calls.items()}
+            del timed_answering_calls
+            del queued_calls_without_irrelevant_missed_calls
 
     # Подключаемся к Redis и делаем запросы
     conn_redis = redis.StrictRedis(**config["redis"])
-
-    g = conn_redis.smembers("online_agents")
-    print(g)
-
-    online_agents = conn_redis.hgetall("online_agents:substate:normal")
-    new_online_agents = {}
-    for item in online_agents.items():
-        key, value = map(decode_bytes_in_str, item)
-        new_online_agents[key] = value
-    data_json["online_agents:substate:normal"] = new_online_agents
 
     # Расчетное время ожидания (EWT) по каждой очереди
     project_ids = conn_redis.smembers("project_config:projects_set")  # Запрашиваем id проктов
@@ -115,9 +100,16 @@ def main():
         ewt[project_id] = {}
         s = str(conn_redis.get("project_config:%s:mean_wait" % project_id))
         s = decode_bytes_in_str(s)
-        ewt[project_id]["ewt"] = s
-        print(s)
-    data_json["projects_id"] = [ewt]
+        ewt[project_id] = s
+        print(ewt)
+
+    # Переменная для формирования JSON файла
+    data_json = {}
+    # Заполняем переменную
+    data_json["sl_instalation"] = sum(sl_projects.values())
+    data_json["projects"] = []
+    for key, value in sl_projects.items():
+        data_json["projects"] += [{key: [{"el": value, "ewt": ewt[key]}]}]
 
     # Создаём json
     data_json["param"] = k
@@ -145,26 +137,3 @@ else:"""
 main()
 
 # Альфа версия (Не смотреть)--------------------------------------------------------------------------------------------
-
-
-"""
-# Расчёт уровня сервиса SL
-with psycopg2.connect(
-    dbname=dataConnPostrgeSQL["dbname"], host=dataConnPostrgeSQL["host"],
-    user=dataConnPostrgeSQL["user"], password=dataConnPostrgeSQL["password"]
-) as conn:
-    with conn.cursor(cursor_factory=DictCursor) as cursor:
-        x = cursor.execute(
-                           "SELECT project_id, count(session_id) \
-                           FROM ns_inbound_call_data \
-                           WHERE is_processed=True and is_processed_after_threshold=False \
-                            GROUP BY project_id"
-        )
-        y = cursor.execute(
-                            "SELECT project_id, count(session_id) \
-                            FROM ns_inbound_call_data \
-                            WHERE is_unblocked=True and is_shortly_abandoned=False \
-                            GROUP BY project_id"
-        )
-        print(str(int(x)/int(y)))
-"""
